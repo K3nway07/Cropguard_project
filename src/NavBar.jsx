d3b671fd46755a897dc6fe3a482b07b2eb86e10e
Pratyush <pratyushbanerjeeadv6@gmail.com>
2025-06-12 15:48:12 +0530

    Node and NavBar changes

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,11 +1,15 @@
 // components/Navbar.jsx
 import "./Navbar.css";
 import { Link } from 'react-router-dom'
+import { Leaf } from 'lucide-react';
+
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-left">
+      <Leaf size={50} color="#ffffff" style={{ marginRight: '8px' }} />
+
         <h1 className="brand-name">CropGuard</h1>
       </div>
       <ul className="navbar-links">
