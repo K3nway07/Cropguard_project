bbb94536e29fca572d12bfec6f78ff067728a373
Pratyush <pratyushbanerjeeadv6@gmail.com>
2025-06-18 16:27:44 +0530

    Complete WebPages

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,6 +1,9 @@
 import './Contact.css'
 import TeamCard from './TeamCard.jsx'
 import pratyush from './Pratyush.jpg'
+import sagnic from './Sagnic.jpeg'
+import jit from './Jit.jpeg'
+import manir from './Manir.jpeg'
 
 function Contact() {
   return (
@@ -16,10 +19,10 @@ function Contact() {
 
       {/* Members in row */}
       <div className="members-section">
-        <TeamCard name="Member 1" designation="Member" image={pratyush} />
-        <TeamCard name="Member 2" designation="Member" image={pratyush} />
-        <TeamCard name="Member 3" designation="Member" image={pratyush} />
-        <TeamCard name="Member 4" designation="Member" image={pratyush} />
+        <TeamCard name="Sagnic Basu" designation="Member" image={sagnic} />
+        <TeamCard name="Pratyush Banerjee" designation="Member" image={pratyush} />
+        <TeamCard name="Manir Ranjan Kundu" designation="Member" image={manir} />
+        <TeamCard name="Jit Mondal" designation="Member" image={jit} />
       </div>
     </div>
   )
